7d55d297129bc3db3967d2112b9680f6908a99a3
Carl Mitander <carl@oniq.solutions>
2021-02-10 13:20:17 +0100

    fixed files

---
+++ frontend/src/components/Playlist.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Playlist</h1>
+    <ul>
+      <li v-for="song in playlist" :key="song">
+        {{ song }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Playlist",
+    data() {
+      return {
+          playlist: {
+              name: String,
+              songs: [],
+          }
+      };
+    },
+    async created() {
+      const res = await fetch("/api/playlist");
+      const data = await res.json();
+      this.playlist = data;
+    },
+  };
+</script>
+
+<style scoped>
+</style>
