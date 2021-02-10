274ffff51df4166a44a81c097752c6addc55cb50
Carl Mitander <carl@oniq.solutions>
2021-02-10 12:52:23 +0100

    PlayListList basic component

---
+++ frontend/src/components/PlaylistList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>PlaylistList</h1>
+    <ul>
+      <li v-for="list in playlists" :key="list">
+        {{ list }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "PlaylistList",
+    data() {
+      return {
+          playlists: []
+      };
+    },
+    async created() {
+      const res = await fetch("/api/playlist");
+      const data = await res.json();
+      this.playlists = data;
+    },
+  };
+</script>
+
+<style scoped>
+</style>
