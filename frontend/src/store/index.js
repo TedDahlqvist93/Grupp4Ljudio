0dfa32ff065507e5efa0fbc6d1dcfb4877a41b5a
Carl Mitander <carl@oniq.solutions>
2021-02-16 16:56:57 +0100

    updated store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,12 +4,74 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        user: {
+            id: null,
+            email: '',
+            loggedIn: false,
+        },
+        player: {
+            isPlaying: false,
+            showVideo: false,
+        },
+        currentSong: {
+            id: null,
+            name: '',
+            artist: '',
+            album: '',
+        },
+        currentPlayList: {
+            id: null,
+            name: '',
+            songs: [],
+        },
+        allPlaylists: {
+            playlists: []
+        },
+        searchList: {
+            id: null,
+            search: '',
+            songs: [],
+        }
+    },
+    getters: {
+        getIsPlaying: state => {
+            return state.isPlaying;
+        },
+        getShowVideo: state => {
+            return state.showVideo;
+        },
+        getCurrentSong: state => {
+            return state.currentSong;
+        },
+        getCurrentPlaylist: state => {
+            return state.currentPlaylist;
+        },
+        getAllPlaylists: state => {
+            return state.currentPlaylist;
+        }
+    },
+    mutations: {
+        setIsPlaying: (state, status) => {
+            state.isPlaying = status
+        },
+        setShowVideo: (state, status) => {
+            state.showVideo = status;
+        },
+        setCurrentSong: (state, status) => {
+            state.currentSong = status;
+        },
+        setCurrentPlaylist: (state, status) => {
+            state.currentPlaylist = status;
+        },
+        setAllPlaylists: (state, status) => {
+            state.allPlaylists = status;
+        }
+    },
+    actions: {
+        getUserStatus(context) {
+            // update user status here?
+            // e.g login. logout
+        }
+    },
+});
