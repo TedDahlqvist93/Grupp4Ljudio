e90e16474cde65ed849765138bc3e30a68a77934
reddington12 <37213559+Reddington12@users.noreply.github.com>
2021-02-19 00:03:46 +0100

    responsivenes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,7 +11,6 @@
           rounded
           background-color="white"
           color="black"
-          
         ></v-text-field>
       </v-responsive>
       <v-btn
@@ -21,45 +20,52 @@
         dark
         @click="search(query)"
         max-width="120"
-      >Search
+        >Search
       </v-btn>
     </v-app-bar>
 
     <!-- List with playlists  -->
     <v-navigation-drawer app width="300" class="">
-      <SearchResults/>
+      <SearchResults />
     </v-navigation-drawer>
 
     <!-- List with songs -->
     <v-navigation-drawer app clipped right>
-        <Register/>
+      <Register />
     </v-navigation-drawer>
     <!-- navbar for mobilescreen -->
 
     <media-display></media-display>
-    <v-footer app color="transparent" height="140" width="710" inset>
-      <v-app-bar color="pink" rounded height="59" width="710" inset >
-        <v-btn rounded color="gray" width="15%" @click="playPrevious() ">
+    <v-main>
+      <v-card>
+        <span><SearchResults /></span></v-card
+    ></v-main>
+    <v-footer app color="transparent" height="140" inset>
+      <v-app-bar color="#424242" rounded inset>
+        <v-btn color="grey" width="15%" @click="playPrevious()">
           <v-icon>mdi-skip-previous-circle</v-icon>
         </v-btn>
-        <v-btn rounded v-if="!this.$store.state.player.isPlaying" color="gray" width="15%" @click="play(true)">
+        <v-btn
+          v-if="!this.$store.state.player.isPlaying"
+          color="grey"
+          width="15%"
+          @click="play(true)"
+        >
           <v-icon>mdi-play</v-icon>
         </v-btn>
-        <v-btn rounded v-else color="gray" width="15%" @click="play(false)">
+        <v-btn v-else color="grey" width="15%" @click="play(false)">
           <v-icon>mdi-pause</v-icon>
         </v-btn>
-        <v-btn rounded color="gray" width="15%" @click="playNext()">
+        <v-btn color="grey" width="15%" @click="playNext()">
           <v-icon>mdi-skip-next-circle</v-icon>
         </v-btn>
         <v-slider
           min="0"
           max="100"
           value="100"
-          color="white"
           thumb-label
-          append-icon="mdi-volume-high"
-          v-model="value"
-          @input="changeVolume(value)"
+          prepend-icon="mdi-volume-high"
+          @drag="changeVolume()"
         >
         </v-slider>
       </v-app-bar>
@@ -73,7 +79,6 @@ import Playlist from "./components/Playlist.vue";
 import { mapActions, mapGetters } from "vuex";
 import MediaDisplay from "./components/MediaDisplay.vue";
 import Register from "./components/Register.vue";
-
 export default {
   name: "App",
   components: {
@@ -88,41 +93,33 @@ export default {
       value: 1,
       text: "",
       query: "",
-      value: "100"
-    
     };
   },
   computed: {
     color() {
       switch (this.value) {
         case 0:
-          return "pink";
+          return "#880E4F";
         case 1:
-          return "pink";
+          return "teal";
         case 2:
           return "cyan";
         case 3:
           return "indigo";
         default:
-          return "pink";
+          return "#880E4F";
       }
     },
     ...mapGetters(["getSearchList"]),
-
-
   },
   methods: {
     ...mapActions(["searchSong"]),
-    play(bool){
-        this.$store.commit("setIsPlaying", bool)
-      
-    },
-    changeVolume(volume){
-        this.$store.commit("setVolume", volume)
+    play(bool) {
+      this.$store.commit("setIsPlaying", bool);
     },
     search(query) {
-      this.searchSong(query)
-    }
+      this.searchSong(query);
+    },
   },
 };
 </script>
